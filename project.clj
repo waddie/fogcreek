5d82337f84d673ed74ba1a472452320265907fb4
Tom Waddington <tom@implementor.org>
2015-04-25 18:28:05 +0100

    tidy up

--- project.clj
+++ project.clj
@@ -3,6 +3,4 @@
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :main ^:skip-aot fogcreek.core
   :target-path "target/%s"
-  :plugins [[lein-ancient "0.6.7"]
-            [lein-kibit "0.1.2"]]
   :profiles {:uberjar {:aot :all}})
